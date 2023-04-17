0710cd10ee26dd6d8ea08d7c70de4b82664b870e
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-17 12:33:51 +0530

    routing-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {  MaterialModule } from './material/material.module';
 import { MenuBarComponent } from './common/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TestimonialComponent } from './pages/testimonial/testimonial.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HomeComponent,
+    AboutComponent,
+    TestimonialComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
