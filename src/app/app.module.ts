36a0f11bedf4282477f6b3e3abd1706ed7c8ce9f
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-17 12:10:48 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoMaterialModule } from './material/material.module';
+import {  MaterialModule } from './material/material.module';
+import { MenuBarComponent } from './common/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    DemoMaterialModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
