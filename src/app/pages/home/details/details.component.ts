a097500015034fe891d81237f47a1853caa781ea
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-25 11:06:51 +0530

    details-commit

--- src/app/pages/home/details/details.component.ts
+++ src/app/pages/home/details/details.component.ts
@@ -8,8 +8,31 @@ import { HomeService } from 'src/app/services/home.service';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-   
+   id:any;
+   service:any;
+   details:any;
   constructor(private hs:HomeService,private ar:ActivatedRoute){
-     
+     this.ar.params.subscribe(
+       {
+         next: (params)=>{ 
+          this.id =params['id']
+          this.readData()
+         },
+         error: () => this.id = 0
+       }
+     )
+  }
+
+  readData(){
+
+     this.hs.getServiceById(this.id).subscribe({
+        next: (data:any)=> this.service =data,
+        error: ()=> this.service = {}
+     })
+
+     this.hs.getDetails(this.id).subscribe({
+         next: (data:any)=> this.details =data,
+         error: ()=> this.details = {}
+     })
   }
 }
