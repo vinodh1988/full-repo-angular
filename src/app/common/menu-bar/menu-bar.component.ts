e52ecae96d2fbadc2fd14979fb71fd27922b597e
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-26 12:24:47 +0530

    localization

--- src/app/common/menu-bar/menu-bar.component.ts
+++ src/app/common/menu-bar/menu-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-menu-bar',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
+  lang:any= "english"
+  languages:any=[
+    {name:"English",value:"english"},
+    {name:"Tamil",value:"tamil"},
+    {name:"Telugu",value:"telugu"},
+    {name:"Hindi",value:"hindi"}
 
+  ]
+    constructor(private ts:TranslateService){
+        this.ts.use(this.lang)
+    }
+
+    changeLang():void{
+       this.ts.use(this.lang)
+    }
 }
